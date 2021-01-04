fb9dea0cbe33c74a905886558020d824486a9a5c
stevenliu88 <30949641+stevenliu88@users.noreply.github.com>
2021-01-04 17:14:20 +1100

    Add new razor pages

--- BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -19,10 +19,27 @@ namespace BookListRazor.Pages.BookList
         }
 
         public IEnumerable<Book> Books { get; set; }
+
+     
         public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id) 
+        {
+            var book = await _db.Book.FindAsync(id);
+            if (book == null) 
+            {
+                return NotFound();
+            }
+
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+      
 
 
     }
